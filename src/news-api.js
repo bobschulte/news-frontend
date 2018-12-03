57adbe2efeeacc117fd51d4f89424573455b06f4
Megan Farley <megfarley4@gmail.com>
2018-12-03 16:52:43 -0600

    added some stuff to news-api.js

---
+++ src/news-api.js
@@ -0,0 +1,8 @@
+var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          ${API_KEY};
+var req = new Request(url);
+fetch(req)
+    .then(function(response){
+        return response.json();
+    }).then(console.log)
