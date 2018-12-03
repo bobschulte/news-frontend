c46ef86bd82e08746c520fd3da8d884271793615
Robert Schulte <schulte.robertw@outlook.com>
2018-12-03 16:32:56 -0600

    got html & js scripts working with mock json data

--- src/index.js
+++ src/index.js
@@ -2,29 +2,32 @@ const notesList = document.querySelector('#notes-list')
 const update = document.querySelector('#update')
 let articles
 
-fetch('http://localhost:3000/notes')
-  .then(function(resp) {resp.json()})
+fetch('http://localhost:3000/articles')
+  .then(function(resp) {
+    return resp.json()
+  })
   .then(function(result) {
     articles = result
     render()
   })
 
-const render() {
+const render = function() {
   renderNotesList()
   renderUpdate()
 }
 
-const renderNotesList() {
+const renderNotesList = function() {
   notesList.innerHTML = ''
-  articles.forEach(function(story) {
-    renderListItem(story)
+  articles.forEach(function(article) {
+    renderListItem(article)
   })
 }
 
-const renderListItem(story) {
-  const story = notesList.appendChild(document.createElement('li'))
+const renderListItem = function(article) {
+  const listItem = notesList.appendChild(document.createElement('li'))
+  listItem.innerText = `${article.title}`
 }
 
-const renderUpdate() {
-
+const renderUpdate = function() {
+  update.innerHTML = `<h1>THIS STILL WORKS</h1>`
 }
