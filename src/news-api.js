0506964e2c63f7ebb2378ed6c323a7b494b45e53
Megan Farley <megfarley4@gmail.com>
2018-12-04 14:24:56 -0600

    data 3 different ways newsAPI

--- src/news-api.js
+++ src/news-api.js
@@ -1,10 +1,56 @@
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          ${API_KEY};
-var req = new Request(url);
-fetch(req)
-    .then(function(response){
-        return response.json();
-    }).then(console.log)
+//will turn these into one function later
 
-//trying to push to github
+const root = "https://newsapi.org/v2/";
+const headlines_path = "top-headlines?language=en&";
+const sources_path = "sources?&";
+const everything_path = "everything?";
+
+let headlines;
+let sources;
+let keyword_articles;
+let keyword = "cat"; //get this from event listener in nav bar
+
+const fetch_headlines = function() {
+  fetch(`https://newsapi.org/v2/top-headlines?language=en&${API_KEY}`, {
+    headers: {
+      authorization: `Bearer ${API_KEY}`
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      headlines = response;
+      console.log(response);
+    });
+};
+
+const fetch_sources = function() {
+  fetch(`https://newsapi.org/v2/sources?&${API_KEY}`, {
+    headers: {
+      authorization: `Bearer ${API_KEY}`
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      sources = response;
+      console.log(response);
+    });
+};
+
+const fetch_keyword_articles = function() {
+  fetch(`https://newsapi.org/v2/everything?q=${keyword}&${API_KEY}`, {
+    headers: {
+      authorization: `Bearer ${API_KEY}`
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      keyword_articles = response;
+      console.log(response);
+    });
+};
