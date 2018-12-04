93b57f67dfb068b500896c622661fcc6017e164f
Robert Schulte <schulte.robertw@outlook.com>
2018-12-03 19:40:50 -0600

    more html & index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,67 @@
 const navBar = document.querySelector('#navbar')
 const publicationsBar = document.querySelector('#bar-publications')
 const trendingBar = document.querySelector('#bar-trending')
-const restOfPage = document.querySelector('#dynamic-page')
+const featureDiv = document.querySelector('#feature')
+const articleListDiv = document.querySelector('#article-list')
+const endPoint = 'http://localhost:3000/articles'
 
 let articles
+let featuredArticle
 
-fetch('http://localhost:3000/articles')
+fetch(endPoint)
   .then(function(resp) {
     return resp.json()
   })
   .then(function(result) {
     articles = result
+    featuredArticle = articles[1]
     render()
   })
 
 const render = function() {
+  // renderNavBar()
   // renderPublicationsBar()
   // renderTrendingBar()
-  renderRestOfPage()
+  renderFeature(featuredArticle)
+  renderArticleList()
 }
 
-const renderRestOfPage = function() {
-  restOfPage.innerHTML = `<h1>Featured Article: ${articles[0].title}</h1>`
+// const renderNavBar() {
+//
+// }
+
+// const renderPublicationsBar() {
+//
+// }
+
+// const renderTrendingBar() {
+//
+// }
+
+const renderFeature = function(article) {
+  featureDiv.innerHTML = `
+    <img class="feature-img" src="${article.urlToImage}" alt="generic-pic">
+    <h1>${configureTitle.call(article)}</h1>
+  `
+}
+
+const renderArticleList = function() {
+  articleListDiv.innerHTML = ''
+  const articleList = articleListDiv.appendChild(document.createElement('ul'))
+  articleList.setAttribute("id", 'article-list')
+  articleList.setAttribute("class", 'list-unstyled')
+  articles.forEach(function(article) {
+    renderArticle(article, articleList)
+  })
+}
+
+const renderArticle = function(article, articleList) {
+  const articleListItem = articleList.appendChild(document.createElement('li'))
+  articleListItem.setAttribute("class", "media")
+  articleListItem.innerHTML = `
+    <img class="mr-1" src="${article.urlToImage}" alt="generic-pic">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">${configureTitle.call(article)}</h5>
+    </div>
+   `
 }
