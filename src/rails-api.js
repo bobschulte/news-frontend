c0ec971b4f5d51cbea2281c77fe4e4b12f95bde0
Robert Schulte <schulte.robertw@outlook.com>
2018-12-05 16:14:58 -0600

    no errors in browser console

--- src/rails-api.js
+++ src/rails-api.js
@@ -1,16 +1,16 @@
-const railsServer = railsAjax("http://localhost:3000/api/v1")
+const railsServer = ajax("http://localhost:3000/api/v1")
 
 // STORIES DATA
 let stories // we will set this in server.get (for index action)
-let currentStory = {} // we will set this in server.post (for create action)
-let currentStoryId // we will use this as input in server.get (for show action)
+let currentStoryId = 1 // we will use this as input in server.get (for show action)
 let storyToShow = {} // we will set this in server.get (for show action)
+let newStory = {} // we will set this in server.post (for create action)
 
 // COMMENTS DATA
 let comments // we will set this in server.get (for index action)
-let currentComment = {} // we will set this in server.post (for create action)
-let currentCommentId // we will use this as input in server.get (for show action)
+let currentCommentId = 1 // we will use this as input in server.get (for show action)
 let commentToShow = {} // we will set this in server.get (for show action)
+let newComment = {} // we will set this in server.post (for create action)
 
 // STORIES API REQUESTS
 railsServer.get('/stories')
@@ -27,10 +27,10 @@ railsServer.get(`/stories/${currentStoryId}`)
         })
     })
 
-railsServer.post('/stories', currentStory)
+railsServer.post('/stories', newStory)
     .then(function (result) {
         update(function () {
-            currentStory.id = result.id;
+            newStory.id = result.id;
         })
     })
 
@@ -50,10 +50,10 @@ railsServer.get(`/comments/${currentCommentId}`)
         })
     })
 
-railsServer.post('/comments', currentComment)
+railsServer.post('/comments', newComment)
     .then(function(result) {
         update(function() {
-            currentComment.id = result.id;
+            newComment.id = result.id;
         })
     })
 
